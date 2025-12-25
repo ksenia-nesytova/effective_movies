cacc509931be1b38190e25391a6e4c482effedb5
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-12-25 16:51:07 +0700

    add: Movie model

---
+++ src/app/movies/models/movie.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+    id: string;
+    title: string;
+    year: number;
+    director: string;
+    cast: string[];
+    country: string;
+    rating: number;
+    genres: string[];
+    description: string;
+    coverImage: string;
+}
