2db691fa5e114e4933f5ee949d2b24823ca0663a
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-12-26 21:56:40 +0700

    add: searchbar to header

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Search } from '@app/movies/components/movie-search/search';
+import { MovieSearch } from "@app/movies/components/movie-search/movie-search";
+
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, MovieSearch],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  private searchService = inject(Search);
 
+  onSearch(query: string) {
+    this.searchService.query.set(query);
+  }
 }
