ee25e036507ac418834c290fa8f5d8d9b9a9ff63
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-12-28 19:46:37 +0700

    add: movie-card.ts

---
+++ src/app/movies/components/movie-card/movie-card.ts
@@ -0,0 +1,34 @@
+import { Component, inject, input, InputSignal, OnInit, signal, WritableSignal } from '@angular/core';
+import { Movie } from '@app/movies/models/movie';
+import { NgOptimizedImage } from "@angular/common";
+import { Router } from '@angular/router';
+
+const DEFAULT_IMAGE = 'assets/movies/USS_Enterprise.png';
+
+@Component({
+  selector: 'app-movie-card',
+  imports: [NgOptimizedImage],
+  templateUrl: './movie-card.html',
+  styleUrl: './movie-card.scss',
+})
+export class MovieCard implements OnInit {
+  private readonly router = inject(Router);
+
+  public movie: InputSignal<Movie | null> = input<Movie | null>(null);
+  protected coverImage: WritableSignal<string> = signal(DEFAULT_IMAGE);
+
+  ngOnInit() {
+    this.coverImage.set(this.getCoverImage());
+  }
+
+  protected getCoverImage(): string {
+    return this.movie()?.coverImage || DEFAULT_IMAGE;
+  }
+
+  protected showDetails(): void {
+    const id = this.movie()?.id;
+    if (id) {
+      this.router.navigate(['/movies', id]);
+    }
+  }
+}
---
+++ src/app/movies/components/movie-details/movie-details.ts
@@ -0,0 +1,59 @@
+import { Component, computed, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '@app/movies/models/movie';
+import { Movies } from '@app/movies/services/movies';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { NgOptimizedImage } from "@angular/common";
+
+const DEFAULT_IMAGE = 'assets/movies/USS_Enterprise.png';
+
+@Component({
+  selector: 'app-movie-details',
+  imports: [MatProgressSpinner, NgOptimizedImage],
+  templateUrl: './movie-details.html',
+  styleUrl: './movie-details.scss',
+})
+export class MovieDetails {
+  private readonly moviesService = inject(Movies);
+  private readonly route = inject(ActivatedRoute);
+  private readonly destroyRef = inject(DestroyRef);
+
+  protected isLoading = signal(true);
+  protected isFailed = signal(false);
+
+  protected movie = signal<Movie | null>(null);
+  protected coverImage = computed(() =>
+    this.movie()?.coverImage ?? DEFAULT_IMAGE
+  );
+
+  constructor() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.loadMovie(id);
+    } else {
+      this.isFailed.set(true);
+      this.isLoading.set(false);
+    }
+  }
+
+  protected loadMovie(id: string): void {
+    this.isFailed.set(false);
+    this.isLoading.set(true);
+
+    this.moviesService.getMovieById(id)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (movie) => {
+          this.movie.set(movie);
+          this.isLoading.set(false);
+        },
+        error: (error) => {
+          console.error('Error fetching movie details:', error);
+          this.isFailed.set(true);
+          this.isLoading.set(false);
+        }
+      });
+  }
+}
