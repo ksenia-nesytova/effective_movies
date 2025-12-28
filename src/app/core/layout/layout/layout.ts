c0822bc949f3993b1e2a32752574e1330f09a6de
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-12-28 21:09:10 +0700

    upd: moved header to layout folder

--- src/app/core/layout/layout/layout.ts
+++ src/app/core/layout/layout/layout.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Header } from "@app/core/header/header";
+import { Header } from "@app/core/layout/header/header";
 
 @Component({
   selector: 'app-layout',
