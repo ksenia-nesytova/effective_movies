a37304a5f209997ae7aeec259b45f8a1ffffb0a4
Ksenia Nesytova <passedbyabyss@gmail.com>
2025-12-28 20:33:35 +0700

    upd: removed unused import

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, inject, output } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Search } from '@app/movies/components/movie-search/search';
 import { MovieSearch } from "@app/movies/components/movie-search/movie-search";
